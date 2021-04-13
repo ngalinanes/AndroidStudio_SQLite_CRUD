57790c396d3d47fc5c9dc9980dee63c9aacb4bd2
Nicolas Galiñanes <nicolas.galinianes@gmail.com>
2021-04-13 19:37:49 -0300

    Se crea el controlador de SQLite. Se creo el metodo de registro de ciudades. Se creo el metodo de consulta de datos por ciudad.

---
+++ app/src/main/java/com/example/tp2/DBHelper.java
@@ -0,0 +1,61 @@
+package com.example.tp2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.EditText;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME = "Cities.db";
+
+    public DBHelper (Context context){
+        super(context, "Cities.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase MyDB) {
+        MyDB.execSQL("create Table cities(city TEXT primary key, country TEXT, people TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDB, int oldVersion, int newVersion) {
+        MyDB.execSQL("drop Table if exists cities");
+    }
+
+    public Boolean insertData(String city, String country, String people){
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("city", city);
+        contentValues.put("country", country);
+        contentValues.put("people", people);
+        long result = MyDB.insert("cities", null, contentValues);
+        if (result==-1) return false;
+        else {
+            return true;
+        }
+    }
+
+    public Boolean checkcity(String city){
+        SQLiteDatabase MyDB = this.getWritableDatabase();
+        Cursor cursor = MyDB.rawQuery("Select * from cities where city = ?", new String[] {city});
+        if (cursor.getCount() > 0) return true;
+        else {
+            return false;
+        }
+    }
+
+
+    public void getcity(EditText citysearch, EditText country, EditText people){
+        String city = citysearch.getText().toString();
+        Cursor res = this.getReadableDatabase().rawQuery("select country,people from cities where city= ?",new String[] {city});
+        citysearch.setText("");
+        while (res.moveToNext()){
+            country.append(res.getString(0));
+            people.append(res.getString(1));
+            citysearch.append(city);
+        }
+    }
+}
--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -2,13 +2,63 @@ package com.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText City, Country, People;
+    Button buttonGetCity, buttonNewCity;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonGetCity = (Button) findViewById(R.id.buttonGetCity);
+        buttonNewCity = (Button) findViewById(R.id.buttonNewCity);
+
+        City = (EditText) findViewById(R.id.city);
+        Country = (EditText) findViewById(R.id.country);
+        People = (EditText) findViewById(R.id.people);
+
+        DB = new DBHelper(this);
+
+        buttonNewCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city = City.getText().toString();
+                String country = Country.getText().toString();
+                String people = People.getText().toString();
+
+                if (city.equals("") || country.equals("") || people.equals(""))
+                    Toast.makeText(MainActivity.this, "Por favor, completa todos los campos.", Toast.LENGTH_SHORT).show();
+                else {
+                    Boolean checkcity = DB.checkcity(city);
+                    if (checkcity)
+                        Toast.makeText(MainActivity.this, "Esa ciudad ya se encuentra cargada.", Toast.LENGTH_SHORT).show();
+                    else {
+                        Boolean insert = DB.insertData(city, country, people);
+                        if (insert)
+                            Toast.makeText(MainActivity.this, "Se añadió la ciudad con éxito.", Toast.LENGTH_SHORT).show();
+                        else {
+                            Toast.makeText(MainActivity.this, "Ocurrio un error al añadir la ciudad.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+
+        buttonGetCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DB.getcity(City, Country, People);
+            }
+        });
     }
 }
